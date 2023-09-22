27b5b06324e7cccbcdb97bf41af271c630069502
Carlos Eduardo <carlospn200406@gmail.com>
2023-09-22 15:32:57 -0300

    feat: add header feature

---
+++ src/app/modules/authentication/features/header-auth/header-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-auth',
+  templateUrl: './header-auth.component.html',
+  styleUrls: ['./header-auth.component.scss']
+})
+export class HeaderAuthComponent {
+
+}
---
+++ src/app/modules/authentication/features/header-auth/header-auth.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderAuthComponent } from './header-auth.component';
+
+@NgModule({
+  declarations: [HeaderAuthComponent],
+  imports: [CommonModule],
+})
+export class HeaderAuthModule {}
