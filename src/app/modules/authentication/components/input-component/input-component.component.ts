36b2894f0c4c8787921ee1133491d67a67400c3b
eduardonunespp <carlospn200406@gmail.com>
2023-10-23 10:46:57 -0300

    feat: add control value accessor

--- src/app/modules/authentication/components/input-component/input-component.component.ts
+++ src/app/modules/authentication/components/input-component/input-component.component.ts
@@ -1,11 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'td-input',
   templateUrl: './input-component.component.html',
   styleUrls: ['./input-component.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() label: string = '';
   @Input() type: string = '';
+  @Input() name: string = '';
+  value: any;
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    // Implemente a lógica para definir o estado desabilitado, se necessário
+  }
+
+  onInputChange(event: any) {
+    this.value = event.target.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
 }
--- src/app/modules/authentication/components/input-component/input-component.module.ts
+++ src/app/modules/authentication/components/input-component/input-component.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input-component.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [InputComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule ],
   exports: [InputComponent],
 })
 export class InputComponentModule {}
