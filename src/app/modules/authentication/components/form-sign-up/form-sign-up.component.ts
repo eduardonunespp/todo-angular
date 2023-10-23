d3c51b76653d560b32649c6b2061749e53fb8748
eduardonunespp <carlospn200406@gmail.com>
2023-10-23 10:47:11 -0300

    feat: add

--- src/app/modules/authentication/components/form-sign-up/form-sign-up.component.ts
+++ src/app/modules/authentication/components/form-sign-up/form-sign-up.component.ts
@@ -1,23 +1,63 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms'
+import { msg } from '../../utils'
+import { ImsgError } from '../../types'
 
 @Component({
   selector: 'td-form-sign-up',
   templateUrl: './form-sign-up.component.html',
   styleUrls: ['./form-sign-up.component.scss']
 })
+
 export class FormSignUpComponent {
 
   @Input() title: string = ''
+  msg: ImsgError = msg
 
   constructor(private fb: FormBuilder){}
 
+  passwordPattern = /^.{8,}$/;
+
+  passwordLengthValidator(minLength: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value || control.value.length < minLength) {
+        return { passwordLength: true };
+      }
+      return null;
+    };
+  }
+
+  passwordsMatch(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
+  
+    if (password !== passwordConfirm) {
+      control.get('passwordConfirm')?.setErrors({ match: true });
+      return { match: true };
+    } else {
+      control.get('passwordConfirm')?.setErrors(null);
+      return null;
+    }
+  }
+
   signUpForm: FormGroup = this.fb.group({
     name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    password: ['', [Validators.required]],
-    passwordConfirm: ['', [Validators.required]]
-  })
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(this.passwordPattern), this.passwordLengthValidator(8)]],
+    passwordConfirm: ['', [Validators.required, Validators.pattern(this.passwordPattern)]]
+  },
+    {validators: this.passwordsMatch}
+  )
+
+  isInvalid(inputName: string, validatorName: string) {
+    const formControl: any = this.signUpForm.get(inputName);
+    if (formControl.errors !== null) {
+      return (
+        formControl.errors[validatorName] &&
+        this.signUpForm.get(inputName)?.touched
+      );
+    }
+  }
 
 
 
--- src/app/modules/authentication/components/form-sign-up/form-sign-up.module.ts
+++ src/app/modules/authentication/components/form-sign-up/form-sign-up.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormSignUpComponent } from './form-sign-up.component';
-import { ButtonComponentModule, InputComponentModule } from '../'
-import { ReactiveFormsModule } from '@angular/forms'
-
+import { ButtonComponentModule, InputComponentModule } from '../';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
-  declarations: [
-    FormSignUpComponent
-  ],
+  declarations: [FormSignUpComponent],
   imports: [
-    CommonModule, ButtonComponentModule, InputComponentModule, ReactiveFormsModule
+    CommonModule,
+    ButtonComponentModule,
+    InputComponentModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule,
+
+
   ],
-  exports: [FormSignUpComponent]
+  exports: [FormSignUpComponent],
 })
-export class FormSignUpModule { }
+export class FormSignUpModule {}
