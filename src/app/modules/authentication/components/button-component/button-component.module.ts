b3251e2eafe1814b179b1aa6fb2466c0c7b2f809
Carlos Eduardo <carlospn200406@gmail.com>
2023-10-26 18:38:49 -0300

    feat: initial add spinner in button

--- src/app/modules/authentication/components/button-component/button-component.module.ts
+++ src/app/modules/authentication/components/button-component/button-component.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ButtonComponent } from './button.component';
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   exports: [ButtonComponent],
 })
 export class ButtonComponentModule {}
