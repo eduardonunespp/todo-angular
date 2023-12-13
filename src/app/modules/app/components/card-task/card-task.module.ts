6d796ec5894901bd47e0c26311b6feccc6aa46ed
eduardonunespp <carlospn200406@gmail.com>
2023-12-13 13:52:52 -0300

    feat: add

--- src/app/modules/app/components/card-task/card-task.module.ts
+++ src/app/modules/app/components/card-task/card-task.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { CardTaskComponent } from './card-task.component';
 import { AddIconModule, CheckIconModule, DeleteIconModule, EditIconModule, ListIconModule, TimeIconModule } from 'src/app/shared';
+import { NgIconsModule } from '@ng-icons/core';
+import { heroCheckCircleSolid } from '@ng-icons/heroicons/solid';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { AddIconModule, CheckIconModule, DeleteIconModule, EditIconModule, ListI
     EditIconModule,
     CheckIconModule,
     DeleteIconModule,
+    NgIconsModule.withIcons({ heroCheckCircleSolid }),
     
   ],
   exports: [
