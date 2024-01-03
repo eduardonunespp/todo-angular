7fa8472ca294870e883b889ccfdafa9b4bd64268
eduardonunespp <carlospn200406@gmail.com>
2024-01-02 23:36:31 -0300

    feat: add angular particles

--- src/app/modules/authentication/features/container-auth/container-auth.component.ts
+++ src/app/modules/authentication/features/container-auth/container-auth.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MoveDirection, ClickMode, HoverMode, OutMode, Container, Engine } from "tsparticles-engine";
+
+import { loadSlim } from "tsparticles-slim"; 
+
 
 @Component({
   selector: 'td-container-auth',
@@ -6,5 +10,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./container-auth.component.scss'],
 })
 export class ContainerAuthComponent {
+  id = "tsparticles"
+
+  particlesOptions = {
+    background: {
+        color: {
+            value: "",
+        },
+    },
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: ClickMode.push,
+            },
+            onHover: {
+                enable: false,
+                mode: HoverMode.repulse,
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#27c498",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: false,
+            opacity: 0.5,
+            width: 1,
+        },
+        move: {
+            direction: MoveDirection.none,
+            enable: true,
+            outModes: {
+                default: OutMode.bounce,
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
+        },
+    },
+    detectRetina: true,
+};
+
+particlesLoaded(container: Container): void {
+    console.log(container);
+}
+
+async particlesInit(engine: Engine): Promise<void> {
+    await loadSlim(engine);
+}
   
 }
--- src/app/modules/authentication/features/container-auth/container-auth.module.ts
+++ src/app/modules/authentication/features/container-auth/container-auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgParticlesModule } from "ng-particles";
 import { ContainerAuthComponent } from './container-auth.component';
 import { InputComponentModule, ButtonComponentModule } from '../../components';
 
 @NgModule({
   declarations: [ContainerAuthComponent],
-  imports: [CommonModule, InputComponentModule, ButtonComponentModule],
+  imports: [CommonModule, InputComponentModule, ButtonComponentModule, NgParticlesModule],
   exports: [ContainerAuthComponent],
 })
 export class ContainerAuthModule {}
